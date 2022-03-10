b755ee2f3cbc006c4c7a12215b104549fdb07f46
Esther Inyang <estherinyang07@gmail.com>
2022-03-10 19:47:05 +0100

    first commit here

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const name = document.querySelector(".name")
+const city = document.querySelector(".city")
+const age = document.querySelector(".age")
+const activityNumber = document.querySelector(".activity-number")
+const activityName = document.querySelector(".activity-name")
+
+
